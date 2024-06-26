44dd8315aef61b04d48b666c9b943525b9c8173d
Pavel Fateev <pasha@autokitteh.com>
2024-06-26 10:40:42 -0700

    address code review comments

--- jira_to_google_calendar/program.py
+++ jira_to_google_calendar/program.py
@@ -18,36 +18,37 @@ import autokitteh
 from autokitteh.google import google_calendar_client
 
 
-CALENDAR_CONNECTION_NAME = "my_googlecalendar"
+CALENDAR_CONNECTION_NAME = "my_google_calendar"
 JIRA_CONNECTION_NAME = "my_jira"
 
 
 def on_jira_issue_created(event):
     """Workflow's entry-point, triggered by an incoming Jira event."""
     details = _extract_issue_details(event.data.issue)
-    cal = google_calendar_client(CALENDAR_CONNECTION_NAME)
-    _create_calendar_event(cal, details)
+    _create_calendar_event(details)
 
 
 def _extract_issue_details(issue):
     base_url = os.getenv(JIRA_CONNECTION_NAME + "__BaseURL")
     desc = f"Link to Jira Issue: {base_url}/browse/{issue.key}\n\n"
-    issue_details = {
-        "description": desc + issue.fields.description,
-        "duedate": issue.fields.duedate,
-        "summary": issue.fields.summary,
-    }
+    issue_details = autokitteh.AttrDict(
+        {
+            "description": desc + issue.fields.description,
+            "duedate": issue.fields.duedate,
+            "summary": issue.fields.summary,
+        }
+    )
     return issue_details
 
 
 @autokitteh.activity
-def _create_calendar_event(cal, issue_details):
+def _create_calendar_event(issue_details):
     start_time = datetime.now()
     end_time = start_time + timedelta(minutes=30)
 
     event = {
-        "summary": issue_details["summary"],
-        "description": issue_details["description"],
+        "summary": issue_details.summary,
+        "description": issue_details.description,
         "start": {
             "dateTime": start_time.isoformat(),
             "timeZone": "America/Los_Angeles",
@@ -65,5 +66,6 @@ def _create_calendar_event(cal, issue_details):
         },
     }
 
+    cal = google_calendar_client(CALENDAR_CONNECTION_NAME)
     event = cal.events().insert(calendarId="primary", body=event).execute()
     print("Event created: %s" % (event.get("htmlLink")))
