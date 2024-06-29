e2c56663c637a544406e2acc6d823f0723d3ce62
Pasha Fateev <pasha@autokitteh.com>
2024-06-28 17:41:35 -0700

    ENG-962 Improvements on Jira to Cal demo (#6)
    Related to this [PR](https://github.com/autokitteh/autokitteh/pull/433)
    without which this code will not run.
    
    ---------
    
    Co-authored-by: Daniel Abraham <daniel@autokitteh.com>

--- jira_to_google_calendar/program.py
+++ jira_to_google_calendar/program.py
@@ -15,40 +15,25 @@ from datetime import datetime, timedelta
 import os
 
 import autokitteh
+from autokitteh.atlassian import get_base_url
 from autokitteh.google import google_calendar_client
-
-
-CALENDAR_CONNECTION_NAME = "my_google_calendar"
-JIRA_CONNECTION_NAME = "my_jira"
 
 
 def on_jira_issue_created(event):
     """Workflow's entry-point, triggered by an incoming Jira event."""
-    details = _extract_issue_details(event.data.issue)
-    _create_calendar_event(details)
-
-
-def _extract_issue_details(issue):
-    base_url = os.getenv(JIRA_CONNECTION_NAME + "__BaseURL")
-    desc = f"Link to Jira Issue: {base_url}/browse/{issue.key}\n\n"
-    issue_details = autokitteh.AttrDict(
-        {
-            "description": desc + issue.fields.description,
-            "duedate": issue.fields.duedate,
-            "summary": issue.fields.summary,
-        }
-    )
-    return issue_details
+    _create_calendar_event(event.data.issue.fields, event.data.issue.key)
 
 
 @autokitteh.activity
-def _create_calendar_event(issue_details):
-    start_time = datetime.strptime(issue_details.duedate, "%Y-%m-%d")
+def _create_calendar_event(issue, key):
+    start_time = datetime.strptime(issue.duedate, "%Y-%m-%d")
     end_time = start_time + timedelta(minutes=30)
+    url = get_base_url(os.getenv("JIRA_CONNECTION_NAME"))
+    link = f"Link to Jira Issue: {url}/browse/{key}\n\n"
 
     event = {
-        "summary": issue_details.summary,
-        "description": issue_details.description,
+        "summary": issue.summary,
+        "description": link + issue.description,
         "start": {
             "dateTime": start_time.isoformat(),
             "timeZone": "America/Los_Angeles",
@@ -66,6 +51,6 @@ def _create_calendar_event(issue_details):
         },
     }
 
-    cal = google_calendar_client(CALENDAR_CONNECTION_NAME)
+    cal = google_calendar_client(os.getenv("CALENDAR_CONNECTION_NAME"))
     event = cal.events().insert(calendarId="primary", body=event).execute()
     print("Event created: %s" % (event.get("htmlLink")))
