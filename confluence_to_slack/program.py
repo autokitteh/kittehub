da889d8b83f52c9d782bfb55180eb5978757f55a
Pasha Fateev <pasha@autokitteh.com>
2024-06-28 19:24:15 -0700

    ENG-1080: Remove AttrDict and move constants into .yaml (#7)

--- confluence_to_slack/program.py
+++ confluence_to_slack/program.py
@@ -8,52 +8,37 @@ Workflow:
     4. Notify: send a message to a Slack channel with a snippet of the page
 """
 
-import autokitteh
+import os
+
 from autokitteh.atlassian import confluence_client
 from autokitteh.slack import slack_client
-
-
-AK_CONFLUENCE_CONNECTION = "my_confluence"
-AK_SLACK_CONNECTION = "my_slack"
-
-CONFLUENCE_LABEL = ""  # TODO: Replace with your Confluence label name
-SLACK_CHANNEL = ""  # TODO: Replace with your Slack channel name or ID
-SNIPPET_LENGTH = 150
 
 
 def on_confluence_page_created(event):
     """Workflow's entry-point."""
 
-    confluence = confluence_client(AK_CONFLUENCE_CONNECTION)
+    confluence = confluence_client(os.getenv("AK_CONFLUENCE_CONNECTION"))
     page_id = event.data.page.id
 
     # Ignore pages without the specified label.
     labels = confluence.get_page_labels(page_id)["results"]
-    if not any(label["name"] == CONFLUENCE_LABEL for label in labels):
+    if not any(label["name"] == os.getenv("CONFLUENCE_LABEL") for label in labels):
         return
 
     # Read the page body.
     res = confluence.get_page_by_id(page_id, expand="body.view, include-labels")
     html_body = res["body"]["view"]["value"]
 
-    page = autokitteh.AttrDict(
-        {
-            "snippet": f"```{html_body[:SNIPPET_COUNT]}\n```",
-            "link": event.data.page.self,
-            "space": event.data.page.spaceKey,
-            "title": event.data.page.title,
-        }
-    )
-
-    _send_slack_message(page)
+    _send_slack_message(event.data.page, html_body)
 
 
-def _send_slack_message(page):
-    slack = slack_client(AK_SLACK_CONNECTION)
+def _send_slack_message(page, html_body):
+    slack = slack_client(os.getenv("AK_SLACK_CONNECTION"))
+    snippet_length = int(os.getenv("SNIPPET_LENGTH"))
     message = f"""
-    A new page has been created in the `{page.space}` space.
+    A new page has been created in the `{page.spaceKey}` space.
     *Title*: {page.title}
-    *Snippet*: {page.snippet}
-    <{page.link}|Link to page>
+    *Snippet*: ```{html_body[:snippet_length]}\n```
+    <{page.self}|Link to page>
     """
-    slack.chat_postMessage(channel=SLACK_CHANNEL, text=message)
+    slack.chat_postMessage(channel=os.getenv("SLACK_CHANNEL"), text=message)
