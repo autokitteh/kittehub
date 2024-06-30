08ddcecace3d270294b84bd04a6a81c6a43ac55f
Daniel Abraham <daniel@autokitteh.com>
2024-06-30 08:44:40 +0300

    Confluence project tweaks, like other projects (#12)

--- confluence_to_slack/program.py
+++ confluence_to_slack/program.py
@@ -16,24 +16,24 @@ from autokitteh.slack import slack_client
 
 def on_confluence_page_created(event):
     """Workflow's entry-point."""
-
-    confluence = confluence_client(os.getenv("AK_CONFLUENCE_CONNECTION"))
+    confluence = confluence_client("confluence_connection")
     page_id = event.data.page.id
 
-    # Ignore pages without the specified label.
-    labels = confluence.get_page_labels(page_id)["results"]
-    if not any(label["name"] == os.getenv("CONFLUENCE_LABEL") for label in labels):
+    # Ignore pages without the filter label, if specified.
+    page_labels = confluence.get_page_labels(page_id)["results"]
+    label_names = [label["name"] for label in page_labels]
+    if os.getenv("FILTER_LABEL") not in label_names + [""]:
+        print(f"Filter label not found in page: {label_names}")
         return
 
     # Read the page body.
-    res = confluence.get_page_by_id(page_id, expand="body.view, include-labels")
+    res = confluence.get_page_by_id(page_id, expand="body.view")
     html_body = res["body"]["view"]["value"]
 
     _send_slack_message(event.data.page, html_body)
 
 
 def _send_slack_message(page, html_body):
-    slack = slack_client(os.getenv("AK_SLACK_CONNECTION"))
     snippet_length = int(os.getenv("SNIPPET_LENGTH"))
     message = f"""
     A new page has been created in the `{page.spaceKey}` space.
@@ -41,4 +41,6 @@ def _send_slack_message(page, html_body):
     *Snippet*: ```{html_body[:snippet_length]}\n```
     <{page.self}|Link to page>
     """
+
+    slack = slack_client("slack_connection")
     slack.chat_postMessage(channel=os.getenv("SLACK_CHANNEL"), text=message)
