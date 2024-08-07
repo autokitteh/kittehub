5d696316af1ef81753dd1ec3ecd0a8370ea4317f
Daniel Abraham <daniel@autokitteh.com>
2024-08-07 18:26:34 +0300

    feat(ENG-1380): task chain demo project (#36)
    This project demonstrates 3 execution approaches side-by-side: a single
    workflow that includes retries ("basic" + "advanced" modes), and
    event-driven workflows.
    
    Refs: ENG-1380

---
+++ task_chain/event_driven/program.py
@@ -0,0 +1,84 @@
+"""This module uses an event-driven approach for the task-chain project.
+
+A single workflow runs all the tasks, except retries:
+
+1. First workflow:
+   - Trigger: Slack slash command
+   - Task 1 -> Task 2 -> Task 3 (error) -> Workflow error
+2. Second workflow:
+   - Trigger: user clicks the "Retry" button in Slack
+   - Task 3 (retry) -> Task 4 -> Successful workflow completion
+"""
+
+from pathlib import Path
+import random
+
+from autokitteh.slack import slack_client
+
+
+slack = slack_client("slack_conn")
+
+
+def step1():
+    print("Step 1 is doing stuff...")
+
+
+def step2():
+    print("Step 2 is doing stuff...")
+
+
+def step3():
+    print("Step 3 is doing stuff...")
+    if random.choice([True, False]):
+        raise RuntimeError("Something bad happened")
+
+
+def step4():
+    print("Step 4 is doing stuff...")
+
+
+tasks = [step1, step2, step3, step4]
+
+
+def on_slack_slash_command(event):
+    """Use a Slack slash command from a user to start a chain of tasks."""
+    run_tasks(0, event.data.user_id)
+
+
+def run_tasks(start_index, user_id):
+    # Note to the interested reader: it's easy to improve this project
+    # to traverse a dynamic DAG, instead of a simple static list.
+    for i, task in enumerate(tasks):
+        if i >= start_index:
+            run_retriable_task(task, i, user_id)
+
+    message = "Workflow completed successfully :smiley_cat:"
+    slack.chat_postMessage(channel=user_id, text=message)
+
+
+def run_retriable_task(task, i, user_id):
+    try:
+        task()
+    except Exception as e:
+        ask_user_retry_or_abort(task.__name__, e, i, user_id)
+        raise e  # Abort the current workflow.
+
+    message = f"Task `{task.__name__}` completed"
+    slack.chat_postMessage(channel=user_id, text=message)
+
+
+def ask_user_retry_or_abort(task_name, error, i, user_id):
+    message = f"The task `{task_name}` failed: `{error}`"
+    blocks = Path("interactive_message.json.txt").read_text()
+    blocks = blocks.replace("MESSAGE", message).replace("INDEX", str(i))
+    slack.chat_postMessage(channel=user_id, text="Workflow error", blocks=blocks)
+
+
+def on_slack_interaction(event):
+    """Handle the user's response (retry / abort) in a new workflow."""
+    if event.data.actions[0]["value"] == "abort":
+        return
+
+    # This workflow's starting point is a retry of the failed task in the aborted workflow.
+    i = int(event.data.actions[0]["action_id"].split()[-1])
+    run_tasks(i, event.data.user.id)
---
+++ task_chain/single_workflow/basic/program.py
@@ -0,0 +1,78 @@
+"""This module uses a single-workflow approach for the task-chain project.
+
+A single workflow runs all the tasks, including retry loops.
+It handles Slack interactions using runtime event subscriptions.
+"""
+
+from pathlib import Path
+import random
+
+import autokitteh
+from autokitteh.slack import slack_client
+
+
+slack = slack_client("slack_conn")
+
+
+def step1():
+    print("Step 1 is doing stuff...")
+
+
+def step2():
+    print("Step 2 is doing stuff...")
+
+
+def step3():
+    print("Step 3 is doing stuff...")
+    if random.choice([True, False]):
+        raise RuntimeError("Something bad happened")
+
+
+def step4():
+    print("Step 4 is doing stuff...")
+
+
+def on_slack_slash_command(event):
+    """Use a Slack slash command from a user to start a chain of tasks."""
+    user_id = event.data.user_id
+
+    if not run_retriable_task(step1, user_id):
+        return
+    if not run_retriable_task(step2, user_id):
+        return
+    if not run_retriable_task(step3, user_id):
+        return
+    if not run_retriable_task(step4, user_id):
+        return
+
+    message = "Workflow completed successfully :smiley_cat:"
+    slack.chat_postMessage(channel=user_id, text=message)
+
+
+def run_retriable_task(task, user_id) -> bool:
+    result = True
+    while result:
+        try:
+            task()
+            break
+        except Exception as e:
+            result = ask_user_retry_or_abort(task.__name__, e, user_id)
+
+    if result:
+        message = f"Task `{task.__name__}` completed"
+        slack.chat_postMessage(channel=user_id, text=message)
+
+    return result
+
+
+def ask_user_retry_or_abort(task_name, error, user_id) -> bool:
+    sub = autokitteh.subscribe("slack_conn", 'event_type == "interaction"')
+
+    blocks = Path("interactive_message.json.txt").read_text()
+    blocks = blocks.replace("MESSAGE", f"The task `{task_name}` failed: `{error}`")
+    slack.chat_postMessage(channel=user_id, text="Workflow error", blocks=blocks)
+
+    # Wait for and handle the user's response in this workflow.
+    event = autokitteh.next_event(sub)
+    autokitteh.unsubscribe(sub)
+    return event.actions[0]["value"] == "retry"
