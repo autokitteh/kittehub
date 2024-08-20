d31681eb3b09451cb66213c88365ebf3fed554f3
Daniel Abraham <daniel@autokitteh.com>
2024-08-20 19:25:20 +0300

    Move quickstart from samples (#48)

---
+++ quickstart/program.py
@@ -0,0 +1,17 @@
+"""Basic handler for incoming HTTP GET requests."""
+
+import time
+
+
+FIVE_SECONDS = 5
+
+
+def on_http_get(event):
+    print(f"Received {event.data.method} request")
+
+    iterations = int(event.data.params.get("iters", "0"))
+    for i in range(iterations):
+        print("Loop iteration: %d of %d" % (i + 1, iterations))
+        time.sleep(FIVE_SECONDS)
+
+    print(f"Finished processing {event.data.method} request")
