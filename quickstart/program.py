dea99d293e123574b5f572c66afaf305b8a91f16
Daniel Abraham <daniel@autokitteh.com>
2024-08-20 20:12:54 +0300

    Quickstart tweaks

--- quickstart/program.py
+++ quickstart/program.py
@@ -9,9 +9,9 @@ FIVE_SECONDS = 5
 def on_http_get(event):
     print(f"Received {event.data.method} request")
 
-    iterations = int(event.data.params.get("iters", "0"))
+    iterations = int(event.data.url.query.get("iterations", "0"))
     for i in range(iterations):
-        print("Loop iteration: %d of %d" % (i + 1, iterations))
+        print(f"Loop iteration: {i + 1} of {iterations}")
         time.sleep(FIVE_SECONDS)
 
     print(f"Finished processing {event.data.method} request")
