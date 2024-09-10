e63bd857f90d75cc9e140c8e5abc2d17f8ad64ee
Pasha Fateev <pasha@autokitteh.com>
2024-09-09 17:16:54 -0700

    feat(samples): add Discord to Google Sheets logging workflow (#62)

---
+++ discord_to_spreadsheet/program.py
@@ -0,0 +1,24 @@
+"""This program logs Discord message events and the author's username
+into a Google Sheets document.
+"""
+
+import os
+
+from autokitteh.google import google_sheets_client
+
+
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+RANGE_NAME = os.getenv("RANGE_NAME")
+
+sheet = google_sheets_client("googlesheets_conn").spreadsheets().values()
+
+
+def on_discord_message(event):
+    values = [[event.data["author"]["username"], event.data["content"]]]
+
+    sheet.append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption="USER_ENTERED",
+        body={"values": values},
+    ).execute()
