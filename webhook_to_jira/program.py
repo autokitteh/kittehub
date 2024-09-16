ff9c7a2acad979edcc9c498fccca97b00c1ed851
Daniel Abraham <daniel@autokitteh.com>
2024-09-16 11:26:51 -0700

    refactor(webhooks): update projects to use new event fields (#68)
    This is a follow-up for
    https://github.com/autokitteh/autokitteh/pull/674
    
    Refs: ENG-1509, ENG-1517, ENG-1518

--- webhook_to_jira/program.py
+++ webhook_to_jira/program.py
@@ -7,9 +7,6 @@ HTTP API documentation:
 - https://docs.autokitteh.com/integrations/http/events
 """
 
-import json
-from urllib.parse import parse_qsl
-
 from autokitteh.atlassian import jira_client
 
 
@@ -21,10 +18,11 @@ def on_http_request(event):
 
     match event.data.headers.get("Content-Type"):
         case "application/json":
-            _create_jira_issue(json.loads(event.data.body))
+            json_body = event.data.body.json  # Or: json.loads(event.data.body.bytes)
+            _create_jira_issue(json_body)
         case "application/x-www-form-urlencoded":
-            body = event.data.body.decode("utf-8")
-            _create_jira_issue(dict(parse_qsl(body)))
+            form_body = event.data.body.form  # Or: dict(urllib.parse.parse_qsl(body))
+            _create_jira_issue(form_body)
 
 
 def _create_jira_issue(fields):
