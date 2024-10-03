37e628418cb10b138f2610a19a73cecd0a71bf86
Pasha Fateev <pasha@autokitteh.com>
2024-10-03 15:26:52 -0700

    feat: remove slack from gmail sample (#80)
    Co-authored-by: Daniel Abraham <daniel@autokitteh.com>

--- samples/google/gmail/program.py
+++ samples/google/gmail/program.py
@@ -9,144 +9,135 @@ import base64
 import json
 
 from autokitteh.google import gmail_client
-from autokitteh.slack import slack_client
 from googleapiclient.errors import HttpError
 
 
 gmail = gmail_client("gmail_conn").users()
-slack = slack_client("slack_conn")
 
 
-def on_slack_slash_command(event):
-    """Use a Slack slash command to interact with a Gmail mailbox.
+def on_http_get(event):
+    """Handle Gmail interaction via HTTP trigger using query params.
 
-    See: https://api.slack.com/interactivity/slash-commands, and
-    https://api.slack.com/interactivity/handling#message_responses
+    Example URL: "http://localhost:9980/webhooks/<webhook_slug>?cmd=list_drafts"
 
-    In this sample, we expect the slash command's text to be:
-    - "gmail get profile"
-    - "gmail drafts list [optional query]"
-    - "gmail drafts get <draft ID>"
-    - "gmail messages list [optional query]"
-    - "gmail messages get <message ID>"
-    - "gmail messages send <short message to yourself>"
+    Commands:
+    - cmd=get_profile
+    - cmd=list_drafts&query=optional_query
+    - cmd=get_draft&draft_id=<draft_ID>
+    - cmd=list_messages&query=optional_query
+    - cmd=get_message&message_id=<message_ID>
+    - cmd=send_message&text=<message_text>
 
     Args:
-        event: Slack event data.
+        event: HTTP request event data (contains query parameters).
     """
-    for cmd, handler in COMMANDS.items():
-        if event.data.text.startswith(cmd):
-            handler(event.data.user_id, event.data.text[len(cmd) + 1 :])
-            return
+    params = event.data.url.query
+    cmd = params.get("cmd")
+
+    match cmd:
+        case "get_profile":
+            _get_profile()
+        case "list_drafts":
+            _drafts_list(params.get("query", ""))
+        case "get_draft":
+            _drafts_get(params.get("draft_id"))
+        case "list_messages":
+            _messages_list(params.get("query", ""))
+        case "get_message":
+            _messages_get(params.get("message_id"))
+        case "send_message":
+            _messages_send(params.get("text"))
+        case _:
+            return "Unknown command"
 
 
-def _get_profile(slack_channel, _):
-    """https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.html#getProfile
-
-    Args:
-        slack_channel: Slack channel name/ID to post debug messages to.
-        _: Unused suffix of the user's Slack command, if any.
-    """
+def _get_profile():
+    """https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.html#getProfile"""
     resp = gmail.getProfile(userId="me").execute()
-    slack.chat_postMessage(channel=slack_channel, text=resp["emailAddress"])
-    msg = f"Total no. of messages: `{resp['messagesTotal']}`"
-    slack.chat_postMessage(channel=slack_channel, text=msg)
-    msg = f"Total no. of threads: `{resp['threadsTotal']}`"
-    slack.chat_postMessage(channel=slack_channel, text=msg)
-    msg = f"Current History record ID: `{resp['historyId']}`"
-    slack.chat_postMessage(channel=slack_channel, text=msg)
+    print(resp["emailAddress"])
+    print("Total no. of messages:", resp["messagesTotal"])
+    print("Total no. of threads:", resp["threadsTotal"])
+    print("Current History record ID:", resp["historyId"])
 
 
-def _drafts_get(slack_channel, id):
+def _drafts_get(id):
     """https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.drafts.html#get
 
     Args:
-        slack_channel: Slack channel name/ID to post debug messages to.
         id: Required ID of the draft to retrieve.
     """
     try:
         resp = gmail.drafts().get(userId="me", id=id).execute()
     except HttpError as e:
-        slack.chat_postMessage(channel=slack_channel, text=f"Error: `{e.reason}`")
+        print(f"Error: `{e.reason}`")
         return
 
-    msg = f"```\n{json.dumps(resp, indent=2)}\n```"
-    slack.chat_postMessage(channel=slack_channel, text=msg)
+    print(f"```\n{json.dumps(resp, indent=2)}\n```")
 
 
-def _drafts_list(slack_channel, query):
+def _drafts_list(query):
     """https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.drafts.html#list
 
     Args:
-        slack_channel: Slack channel name/ID to post debug messages to.
         query: Optional query, e.g. "is:unread".
     """
     try:
         resp = gmail.drafts().list(userId="me", q=query, maxResults=10).execute()
     except HttpError as e:
-        slack.chat_postMessage(channel=slack_channel, text=f"Error: `{e.reason}`")
+        print(f"Error: `{e.reason}`")
         return
 
-    msg = f"Result size estimate: `{resp['resultSizeEstimate']}`"
-    slack.chat_postMessage(channel=slack_channel, text=msg)
+    print(f"Result size estimate: `{resp['resultSizeEstimate']}`")
 
     for i, d in enumerate(resp.get("drafts", []), start=1):
-        msg = f"{i}\n```\n{json.dumps(d, indent=2)}\n```"
-        slack.chat_postMessage(channel=slack_channel, text=msg)
+        print(f"{i}\n```\n{json.dumps(d, indent=2)}\n```")
 
     next_page_token = resp.get("nextPageToken")
     if next_page_token:
-        msg = f"Next page token: `{next_page_token}`"
-        slack.chat_postMessage(channel=slack_channel, text=msg)
+        print(f"Next page token: `{next_page_token}`")
 
 
-def _messages_get(slack_channel, id):
+def _messages_get(id):
     """https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.messages.html#get
 
     Args:
-        slack_channel: Slack channel name/ID to post debug messages to.
         id: Required ID of the message to retrieve.
     """
     try:
         resp = gmail.messages().get(userId="me", id=id).execute()
     except HttpError as e:
-        slack.chat_postMessage(channel=slack_channel, text=f"Error: `{e.reason}`")
+        print(f"Error: `{e.reason}`")
         return
 
-    msg = f"```\n{json.dumps(resp, indent=2)}\n```"
-    slack.chat_postMessage(channel=slack_channel, text=msg)
+    print(f"```\n{json.dumps(resp, indent=2)}\n```")
 
 
-def _messages_list(slack_channel, query):
+def _messages_list(query):
     """https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.messages.html#list
 
     See also:
     https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.messages.html#list_next
 
     Args:
-        slack_channel: Slack channel name/ID to post debug messages to.
         query: Optional query, e.g. "is:unread".
     """
     try:
         resp = gmail.messages().list(userId="me", q=query, maxResults=10).execute()
     except HttpError as e:
-        slack.chat_postMessage(channel=slack_channel, text=f"Error: `{e.reason}`")
+        print(f"Error: `{e.reason}`")
         return
 
-    msg = f"Result size estimate: `{resp['resultSizeEstimate']}`"
-    slack.chat_postMessage(channel=slack_channel, text=msg)
+    print(f"Result size estimate: `{resp['resultSizeEstimate']}`")
 
     for i, m in enumerate(resp.get("messages", []), start=1):
-        msg = f"{i}\n```\n{json.dumps(m, indent=2)}\n```"
-        slack.chat_postMessage(channel=slack_channel, text=msg)
+        print(f"{i}\n```\n{json.dumps(m, indent=2)}\n```")
 
     next_page_token = resp.get("nextPageToken")
     if next_page_token:
-        msg = f"Next page token: `{next_page_token}`"
-        slack.chat_postMessage(channel=slack_channel, text=msg)
+        print(f"Next page token: `{next_page_token}`")
 
 
-def _messages_send(slack_channel, text):
+def _messages_send(text):
     """https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1.users.messages.html#send
 
     See also: https://developers.google.com/gmail/api/guides/sending
@@ -155,7 +146,6 @@ def _messages_send(slack_channel, text):
     https://github.com/googleworkspace/python-samples/blob/main/gmail/snippet/send%20mail/send_message.py
 
     Args:
-        slack_channel: Slack channel name/ID to post debug messages to.
         text: Short message to send to yourself.
     """
     profile = gmail.getProfile(userId="me").execute()
@@ -172,21 +162,11 @@ def _messages_send(slack_channel, text):
     try:
         gmail.messages().send(userId="me", body={"raw": msg}).execute()
     except HttpError as e:
-        slack.chat_postMessage(channel=slack_channel, text=f"Error: `{e.reason}`")
+        print(f"Error: `{e.reason}`")
         return
 
-    slack.chat_postMessage(channel=slack_channel, text="Sent!")
+    print("Message sent successfully!")
 
 
 def on_gmail_mailbox_change(event):
     pass  # TODO(ENG-1524): Implement this function.
-
-
-COMMANDS = {
-    "gmail get profile": _get_profile,
-    "gmail drafts get": _drafts_get,
-    "gmail drafts list": _drafts_list,
-    "gmail messages get": _messages_get,
-    "gmail messages list": _messages_list,
-    "gmail messages send": _messages_send,
-}
