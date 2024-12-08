c5cabc4cacfcab9210acb14fc5ce9a798e600a3a
Mario Rohana <mario@autokitteh.com>
2024-12-08 12:34:34 +0200

    read me teable fixes

--- scripts/create_project_table.py
+++ scripts/create_project_table.py
@@ -1,73 +1,55 @@
 """
 Process README files to extract metadata, generate a table of content, 
 and update a central README file with aggregated data.
-
-- Extract metadata from the top section of the README file for each project (e.g., between "---" delimiters).
-- Generate table rows summarizing the metadata
-- Insert the generated rows into the project table in the main README file
-
-Classes:
-- ReadmeMetadataProcessor: Extracts and processes metadata from individual README files.
 """
 
 import os
 from pathlib import Path
+import re
 import yaml
 
 ROOT_PATH = Path(__file__).parent.parent
 README_PATH = ROOT_PATH / "README.md"
 
 
-class ReadmeMetadataProcessor:
-    def __init__(self, readme_file: Path):
-        self.readme_file = readme_file
-        self.metadata = self.extract_metadata()
+def extract_metadata(readme_file: Path) -> dict:
+    """Extract metadata from a README file."""
+    metadata = {}
+    lines = readme_file.read_text(encoding="utf-8").splitlines()
 
-    def extract_metadata(self) -> dict:
-        metadata = {}
-        lines = self.readme_file.read_text(encoding="utf-8").splitlines()
+    # Check for metadata block at the top of the README
+    if lines[0].strip() == "---":
+        metadata_lines = []
+        for line in lines[1:]:
+            if line.strip() == "---":
+                break
+            metadata_lines.append(line)
 
-        # Check for metadata block at the top of the README
-        if lines[0].strip() == "---":
-            metadata_lines = []
-            for line in lines[1:]:
-                if line.strip() == "---":
-                    break
-                metadata_lines.append(line)
+        metadata = yaml.safe_load("\n".join(metadata_lines))
 
-            metadata = yaml.safe_load("\n".join(metadata_lines))
+    return metadata
 
-        return metadata
 
-    def get_title(self) -> str:
-        return self.metadata.get("title", None)
+def to_table_row(readme_file: Path, metadata: dict) -> str:
+    """Convert metadata into a table row."""
+    title = metadata.get("title", "")
+    if not title:
+        return ""
 
-    def get_description(self) -> str:
-        return self.metadata.get("description", "No description available.")
+    description = metadata.get("description", "")
+    integrations = ", ".join(metadata.get("integrations", [])) or "None"
+    relative_path = os.path.relpath(os.path.dirname(readme_file), ROOT_PATH)
+    link = f"./{relative_path}/"
 
-    def get_integrations(self) -> str:
-        integrations = self.metadata.get("integrations", [])
-        return ", ".join(integrations) if integrations else "None"
-
-    def to_table_row(self) -> str:
-        title = self.get_title()
-        if not title:
-            return ""
-
-        description = self.get_description()
-        integrations = self.get_integrations()
-        relative_path = os.path.relpath(os.path.dirname(self.readme_file), ROOT_PATH)
-        link = f"./{relative_path}/"
-
-        return f"| [{title}]({link}) | {description} | {integrations} |\n"
+    return f"| [{title}]({link}) | {description} | {integrations} |\n"
 
 
 def generate_readme_table(folder_path: Path) -> list[str]:
     """Generate a list of table rows from README metadata."""
     rows = []
     for readme_file in folder_path.rglob("README.md"):
-        processor = ReadmeMetadataProcessor(readme_file)
-        row = processor.to_table_row()
+        metadata = extract_metadata(readme_file)
+        row = to_table_row(readme_file, metadata)
         if row:
             rows.append(row)
     return rows
@@ -75,13 +57,17 @@ def generate_readme_table(folder_path: Path) -> list[str]:
 
 def insert_rows_to_table(readme_file: Path, new_rows: list[str]) -> None:
     """Insert rows into the table section of the README file."""
+    md = readme_file.read_text(encoding="utf-8")
+    headers = "-->\n| Name | Description | Integration |\n| :--- | :---------- | :---------- |\n<!--"
+    md = re.sub("-->.+<!--", headers, md, flags=re.DOTALL)
+
     lines = readme_file.read_text(encoding="utf-8").splitlines(keepends=True)
 
     start_marker_index = next(
-        i for i, line in enumerate(lines) if line.strip() == "<!--start-table-->"
+        i for i, line in enumerate(lines) if line.strip() == "<!-- START-TABLE -->"
     )
     end_marker_index = next(
-        i for i, line in enumerate(lines) if line.strip() == "<!--end-table-->"
+        i for i, line in enumerate(lines) if line.strip() == "<!-- END-TABLE -->"
     )
 
     # Replace content between the markers
@@ -93,29 +79,6 @@ def insert_rows_to_table(readme_file: Path, new_rows: list[str]) -> None:
     readme_file.write_text("".join(updated_lines), encoding="utf-8")
 
 
-def reset_readme(file_path: Path) -> None:
-    """Reset the README.md file's table, in order to add the new table to it."""
-    lines = file_path.read_text(encoding="utf-8").splitlines()
-
-    new_lines = []
-    keep_table_header = False
-
-    for line in lines:
-        # Check if it's the table header or divider row
-        if line.strip().startswith("| Name") or line.strip().startswith("| :"):
-            keep_table_header = True
-            new_lines.append(line)
-            continue
-
-        if keep_table_header and line.strip().startswith("|"):
-            continue
-
-        new_lines.append(line)
-
-    file_path.write_text("\n".join(new_lines) + "\n", encoding="utf-8")
-
-
 if __name__ == "__main__":
-    reset_readme(README_PATH)
     new_rows = generate_readme_table(ROOT_PATH)
     insert_rows_to_table(README_PATH, new_rows)
