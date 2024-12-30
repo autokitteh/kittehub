36b3961747e6d40abaed90482935bc447ae6629f
Daniel Abraham <daniel@autokitteh.com>
2024-12-30 14:46:29 -0800

    tweak

--- purrr/debug.py
+++ purrr/debug.py
@@ -6,7 +6,6 @@ Decoupled from the "slack_helper" module, to avoid circular imports.
 import os
 import re
 import traceback
-from typing import Iterator
 
 from autokitteh.slack import slack_client
 from slack_sdk.errors import SlackApiError
@@ -37,12 +36,15 @@ def log(msg: str) -> None:
         print(f"DEBUG ERROR: {e}")
 
 
-def _stack_messages() -> Iterator[str]:
+def _stack_messages() -> list[str]:
+    msgs = []
     for file, line, func, text in traceback.extract_stack():
         # Log only frame summaries relating to this project, up to this function.
-        if "/ak-user-" not in file or func == "log":
+        if "/ak-user-" not in file:
             continue
 
         # Display shorter and cleaner paths.
         file = re.sub(r"^.+/ak-user-.+?/", "", file)
-        yield f"```File: {file}, line {line}\nFunc: {func}\n{text}```"
+        msgs.append(f"```File: {file}, line {line}\nFunc: {func}\n{text}```")
+
+    return msgs[:-2]  # Skip the last 2 frames (i.e. this module).
