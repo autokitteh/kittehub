51c20f0a1dc5cc600b19fed27d69866811229f29
Daniel Abraham <daniel@autokitteh.com>
2025-01-09 14:52:28 -0800

    ruff 0.9.0 fixes

--- samples/discord/events/program.py
+++ samples/discord/events/program.py
@@ -5,7 +5,7 @@ Also log the corresponding information using the `autokitteh.discord` client.
 
 
 def on_discord_message_create(event):
-    print(f'User {event.data["author"]["username"]} sent: {event.data["content"]}')
+    print(f"User {event.data['author']['username']} sent: {event.data['content']}")
 
 
 def on_discord_message_update(event):
