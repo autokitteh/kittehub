da7d07d793a58ac5cb19531929569d6f3a7aa62f
Mario Rohana <mario@autokitteh.com>
2025-01-28 13:41:51 +0200

    fixes

--- tests/convention_test.py
+++ tests/convention_test.py
@@ -65,7 +65,6 @@ TEST_CATEGORIES = [
 
 @pytest.mark.parametrize(("folder_path", "category"), TEST_CATEGORIES)
 def test_projects_include_required_category(folder_path, category):
-    """Ensures projects in each folder include the required category."""
     for readme_file, metadata in get_metadata_files(folder_path):
         error = f"Project is missing the {category} category: {readme_file}"
         assert category in metadata["categories"], error
