796e0fea397281be7a332a921df2bba472a9b19f
Itay Donanhirsh <itay@autokitteh.com>
2025-02-10 17:44:59 -0800

    work

---
+++ incidenter/height.py
@@ -0,0 +1,45 @@
+from os import getenv
+from urlparse import urljoin
+
+from autokitteh.height import height_client
+
+
+_LIST_ID = getenv("HEIGHT_LIST_ID")
+
+_ROOT_URL = "https://api.height.app/"
+
+height = height_client("height")
+
+
+if not _LIST_ID:
+    raise ValueError("HEIGHT_LIST_ID is not set")
+
+
+def _post(path: str, data: dict) -> dict:
+    resp = height.post(urljoin(_ROOT_URL, path), json=data)
+    resp.raise_for_status()
+    return resp.json()
+
+
+def create_task(name: str, desc: str, status: str) -> dict:
+    return _post(
+        "tasks",
+        {
+            "name": name,
+            "type": "task",
+            "description": desc,
+            "status": status,
+            "listIds": [_LIST_ID],
+        },
+    )
+
+
+def add_task_message(task_id: str, msg: str):
+    return _post(
+        "activities",
+        {
+            "taskId": task_id,
+            "type": "comment",
+            "message": msg,
+        },
+    )
