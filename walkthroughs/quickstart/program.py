70082bb3ac8b28bcf9a1d9d467ea91f12d0b5163
Pavel Fateev <pasha@autokitteh.com>
2025-03-14 11:42:02 -0700

    add another const and fix bug with missing parameter

--- walkthroughs/quickstart/program.py
+++ walkthroughs/quickstart/program.py
@@ -1,12 +1,13 @@
-"""Basic handler for incoming HTTP requests."""
+"""Handler for manual runs with a simple loop."""
 
 import time
 
 
 FIVE_SECONDS = 5
+ITERATIONS = 5
 
 
-def on_manual_run():
-    for i in range(10):
-        print(f"Loop iteration: {i + 1} of {10}")
+def on_manual_run(_):
+    for i in range(ITERATIONS):
+        print(f"Loop iteration: {i + 1} of {ITERATIONS}")
         time.sleep(FIVE_SECONDS)
