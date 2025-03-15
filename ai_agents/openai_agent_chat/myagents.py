c4b01ee570dc7d51a398449ab02473c2eb99a55a
Itay Donanhirsh <itay@autokitteh.com>
2025-03-14 17:38:43 -0700

    fix

--- ai_agents/openai_agent_chat/myagents.py
+++ ai_agents/openai_agent_chat/myagents.py
@@ -5,19 +5,22 @@ from agents import WebSearchTool
 
 _cats_agent = Agent(
     name="Cats Agent",
-    handoff_description="Specialist agent for questions about cats. Always include the word 'meow' in your responses.",
+    handoff_description="""Specialist agent for questions about cats.
+Always include the word 'meow' in your responses.""",
     instructions="You provide help questions about cats",
 )
 
 _dogs_agent = Agent(
     name="Dogs Agent",
-    handoff_description="Specialist agent for questions about dogs. Always include the word 'woof' in your responses.",
+    handoff_description="""Specialist agent for questions about dogs.
+Always include the word 'woof' in your responses.""",
     instructions="You provide help questions about dogs",
 )
 
 _search_agent = Agent(
     name="Search Agent",
-    handoff_description="Specialist agent for web searches. Explicitly say you found your answer on the web and which url you found it at.",
+    handoff_description="""Specialist agent for web searches. 
+Explicitly say you found your answer on the web and which url you found it at.""",
     instructions="You search the web for answers to queries",
     tools=[WebSearchTool()],
 )
