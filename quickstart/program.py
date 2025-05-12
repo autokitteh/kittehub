b3953f713d23bd9bad2ef08f70a4a112f99eaee2
Mario Rohana <104531719+mario99logic@users.noreply.github.com>
2025-05-12 17:51:50 +0300

    fix: add webhook to quickstart sample (#237)

--- quickstart/program.py
+++ quickstart/program.py
@@ -7,7 +7,7 @@ SLEEP_SECONDS = 1
 ITERATIONS = 5
 
 
-def on_manual_run(_):
+def quickstart(_):
     for i in range(ITERATIONS):
         print(f"Loop iteration: {i + 1} of {ITERATIONS}")
         time.sleep(SLEEP_SECONDS)
