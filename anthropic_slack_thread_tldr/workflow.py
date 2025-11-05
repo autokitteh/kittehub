2c8674ed67dd9d3d1f04718170ec0c640429d623
Itay Donanhirsh <itay@autokitteh.com>
2025-11-04 16:09:20 -0800

    fixes & anthropic

--- anthropic_slack_thread_tldr/workflow.py
+++ anthropic_slack_thread_tldr/workflow.py
@@ -2,17 +2,16 @@
 
 from os import getenv
 
-from anthropic import Anthropic
+from autokitteh.anthropic import anthropic_client
 from autokitteh.slack import slack_client
 
 
 _CMD = "!tldr"
+_MAX_TOKENS = int(getenv("MAX_TOKENS", "1000"))
+_MODEL = getenv("MODEL", "claude-3-5-haiku-20241022")
 
-slack_client = slack_client("slack")
-
-anthropic_client = Anthropic(api_key=getenv("ANTHROPIC_API_KEY"))
-
-_MAX_TOKENS = int(getenv("MAX_TOKENS", 1000))
+_slack_client = slack_client("slack")
+_anthropic = anthropic_client("anthropic")
 
 
 def on_slack_thread_message(event):
@@ -30,16 +29,17 @@ def on_slack_thread_message(event):
     ch, ts, thread_ts = event.data.channel, event.data.ts, event.data.thread_ts
 
     try:
-        slack_client.reactions_add(channel=ch, timestamp=ts, name="thinking_face")
+        _slack_client.reactions_add(channel=ch, timestamp=ts, name="thinking_face")
 
-        msgs = slack_client.conversations_replies(channel=ch, ts=thread_ts).get(
+        msgs = _slack_client.conversations_replies(channel=ch, ts=thread_ts).get(
             "messages", []
         )
 
         msgs = "\n".join(f"- {msg['user']}: {msg['text']}" for msg in msgs)
 
-        message = anthropic_client.messages.create(
+        message = _anthropic.messages.create(
             max_tokens=_MAX_TOKENS,
+            model=_MODEL,
             messages=[
                 {
                     "role": "user",
@@ -47,13 +47,12 @@ def on_slack_thread_message(event):
 Compose a summary of the Slack conversation in this thread.
 {focus}
 
-Use Slack mentions.
+Use Slack mentions, for example: <@U12345678>.
 
 These are the messages in the thread:
 {msgs}""",
                 }
             ],
-            model="claude-3-5-sonnet-latest",
         )
 
         summary = "".join(
@@ -62,14 +61,14 @@ These are the messages in the thread:
 
         print(summary)
 
-        slack_client.reactions_remove(channel=ch, timestamp=ts, name="thinking_face")
-        slack_client.reactions_add(channel=ch, timestamp=ts, name="ok_hand")
+        _slack_client.reactions_remove(channel=ch, timestamp=ts, name="thinking_face")
+        _slack_client.reactions_add(channel=ch, timestamp=ts, name="ok_hand")
 
-        slack_client.chat_postMessage(
+        _slack_client.chat_postMessage(
             channel=ch,
             thread_ts=thread_ts,
             text=summary,
         )
     except:
-        slack_client.reactions_add(channel=ch, timestamp=ts, name="scream")
+        _slack_client.reactions_add(channel=ch, timestamp=ts, name="scream")
         raise
